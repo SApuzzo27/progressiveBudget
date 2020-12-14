c19b4be24654c79cab83e6538c158987f1422ea8
Sam <SApuzzo527@gmail.com>
2020-12-13 21:41:39 -0500

    removed develop folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,6 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/favicon.ico",
   "/manifest.webmanifest",
   "/assets/style.css",
   "/assets/index.js",
